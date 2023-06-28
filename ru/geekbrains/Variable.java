77b13b53a34cde2dfb6c100360561bf47ba94e5e
chuprov-vd <chuprov271@gmail.com>
2023-06-28 14:14:35 +0300

    work variable

---
+++ ru/geekbrains/Variable.java
@@ -0,0 +1,9 @@
+package ru.geekbrains;
+
+public class Variable {
+    public static void main(String[] args) {    // psvm (для создания точки входа в программу)
+    int x = 5;
+    int y = x + 5;
+        System.out.println(y); // sout (быстрая команда для кода печати )
+    }
+}
