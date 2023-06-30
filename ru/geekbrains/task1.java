0228fc48af4519047684f080588365963801a8dc
chuprov-vd <chuprov271@gmail.com>
2023-07-01 01:18:36 +0300

    new task, решение 1.1 и 1.2

--- ru/geekbrains/task1.java
+++ ru/geekbrains/task1.java
@@ -1,2 +1,35 @@
-package ru.geekbrains;public class task1 {
+package ru.geekbrains;
+
+//1) Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+import java.util.Scanner;
+public class task1 {
+    public static void main(String[] args) {
+        task1_2();
+    }
+    static void task1_1(){
+        System.out.println("Введите число n");
+        Scanner scanner = new Scanner(System.in);
+        // для прочитки данных Scanner scanner = new Scanner из консоли (System.in)
+        int num = scanner.nextInt(); // для предачи данных в переменную num
+        int n = num;
+        int n1 = num - 1;
+        while (n1 > 0){
+            n = n1 + n;
+            n1--;
+        }
+        System.out.println(n);
+    }
+    static void task1_2(){
+        System.out.println("Введите число n");
+        Scanner scanner = new Scanner(System.in);
+        // для прочитки данных Scanner scanner = new Scanner из консоли (System.in)
+        int num = scanner.nextInt(); // для предачи данных в переменную num
+        int n = num;
+        int n1 = num - 1;
+        while (n1 > 0){
+            n = n1 * n;
+            n1--;
+        }
+        System.out.println(n);
+    }
 }
