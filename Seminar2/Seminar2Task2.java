f792b0c95ac1c41a92cafc1be630c53b37a65b34
chuprov-vd <chuprov271@gmail.com>
2023-07-14 00:18:26 +0300

    new decision

--- Seminar2/Seminar2Task2.java
+++ Seminar2/Seminar2Task2.java
@@ -1,12 +1,16 @@
 package Seminar2;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
-// Напишите метод который сжимает строку Вход: aaaabbbcdd Выход: a4b3c1d2
+// Напишите метод который сжимает строку Вход: ffffkkkrkrlflcaavvrraaaff Выход: a5c1f7k4l2r4v2
 public class Seminar2Task2 {
     public static void main(String[] args) {
         String wordText = InputString();
-        OutputString(wordText);
+       // OutputString(wordText);
+        System.out.println(newDecision(wordText));
+
 
 
     }
@@ -20,12 +24,12 @@ public class Seminar2Task2 {
         StringBuilder StringText = new StringBuilder("");
         int count = 0;
         int j = 0;
-        char symbol = word.charAt(0);
-        char previChar = word.charAt(0);
         for (int i = 0; i < word.length(); i++ ){
+            char symbol;
             symbol = word.charAt(i);
-            previChar = word.charAt(j);
-            if (symbol == previChar) {
+            char twoChar;
+            twoChar = word.charAt(j);
+            if (symbol == twoChar) {
                 count++;
             }
             else {
@@ -37,4 +41,29 @@ public class Seminar2Task2 {
         StringText.append(word.charAt(word.length() - 1)).append(count);
         System.out.println(StringText);
     }
+    static StringBuilder newDecision(String word){
+        char [] chars = word.toCharArray();
+        Arrays.sort(chars);
+        System.out.println(chars);
+        int count = 0;
+        int j = 0;
+        StringBuilder stringBuilders = new StringBuilder();
+
+        for (int i = 0; i < chars.length; i++ ){
+            if(chars[i] == chars[j]){
+                count++;
+            }
+            else{
+                    char symbol = chars[j];
+                    stringBuilders.append(symbol).append(count);
+                    j = j + count;
+                    count = 1;
+                }
+            }
+        char symbol = chars[j];
+        stringBuilders.append(symbol).append(count);
+        return stringBuilders;
+    }
+
 }
+
