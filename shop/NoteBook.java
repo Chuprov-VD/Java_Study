911ac240572130fd244b01cf8e43645de7137b5f
chuprov-vd <chuprov271@gmail.com>
2023-07-17 00:19:45 +0300

    task shop Done

---
+++ shop/NoteBook.java
@@ -0,0 +1,85 @@
+package shop;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NoteBook {
+
+    // Коллекция данных ноутбука
+
+    private Map<NotebookField, String> data = new HashMap<>();
+
+    /**
+     * Метод для получения значения параметра ноутбука
+     * @param field Параметр ноутбука
+     * @return Значение параметра
+     */
+    public String getValue(NotebookField field){
+        return data.get(field);
+    }
+
+    /**
+     * Метод для задания параметра ноутбука
+     * @param field Параметр ноутбука
+     * @param value Значение параметра
+     */
+    public void setValue(NotebookField field, String value) {
+        data.put(field, value);
+    }
+
+    @Override
+    public String toString() {
+        return shortInfo();
+    }
+
+    @Override
+    public int hashCode() {
+        return  5 * getValue(NotebookField.MANUFACTURER).hashCode()
+                + 11 * getValue(NotebookField.MODEL).hashCode()
+                + 17 * getValue(NotebookField.PRICE).hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if (!(o instanceof NoteBook)) return false;
+        boolean eq = true;
+        NoteBook nb = (NoteBook) o;
+        for (NotebookField field : NotebookField.values()) {
+            if (!(nb.getValue(field).equals(getValue(field)))) eq = false;
+        }
+        return eq;
+    }
+
+    /**
+     * Метод для вывода полной информации о ноутбуке
+     * @return Строка с описанием
+     */
+
+    private String fullInfo() {
+        StringBuilder sb = new StringBuilder();
+        for (NotebookField field : NotebookField.values()) {
+            sb.append(field.descritpion()).append(": ").append(getValue(field)).append("\n");
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Метод для вывода информации о ноутбуке в краткой форме
+     * @return Строка с описанием
+     */
+    private String shortInfo() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getValue(NotebookField.MANUFACTURER)).append(" ")
+                .append(getValue(NotebookField.MODEL)).append(" (")
+                .append(getValue(NotebookField.SCREENDIAGONAL)).append("\" ")
+                .append(getValue(NotebookField.SCREENRESOLUTION)).append("/")
+                .append(getValue(NotebookField.PROCESSORMODEL)).append(" ")
+                .append(getValue(NotebookField.PROCESSORCORENUMBER)).append("x")
+                .append(getValue(NotebookField.PROCESSORCOREFREQUENCY)).append("ГГц/")
+                .append(getValue(NotebookField.RAMSIZE)).append("Гб/")
+                .append(getValue(NotebookField.SSDSIZE)).append("Гб) - ")
+                .append(getValue(NotebookField.PRICE)).append(" руб.");
+        return sb.toString();
+    }
+}
---
+++ shop/NoteBookData.java
@@ -0,0 +1,107 @@
+package shop;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class NoteBookData {
+
+    // Множество ноутбуков
+    Set<NoteBook> set = new HashSet<>();
+
+    public NoteBookData() {
+        loadFromFile();
+    }
+
+    /**
+     * Метод для считывания данных из имеющегося файла
+     */
+    public void loadFromFile() {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("notebooks.csv"));
+            String str = "";
+            // Считываем данные. Порядок столбцов в файле совпадают с порядком значений в NotebookFields
+            while ((str = bufferedReader.readLine()) != null) {
+                NoteBook nb = new NoteBook();
+                String[] data = str.split(";");
+                for (int i = 0; i < NotebookField.values().length; i++) {
+                    nb.setValue(NotebookField.values()[i], data[i]);
+                }
+                set.add(nb);
+            }
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    /**
+     * Метод для вывода всего множества ноутбуков на экран
+     */
+    public void showAll() {
+        show(set);
+    }
+
+    /**
+     * Метод для вывода множества соответсвующих фильтру ноутбуков на экран
+     */
+    public void showFiltered(NoteBookFilter filter) {
+        Set<NoteBook> set  = getFiltered(filter);
+        if (set.isEmpty()) {
+            System.out.println("Ничего не найдено :(");
+        } else {
+            show(getFiltered(filter));
+        }
+    }
+
+    /**
+     * Метод для получения списка доступных значений для заданного параметра ноутбука
+     * @param field Параметр ноутбука
+     * @return Список доступных значений
+     */
+    public List<String> getAllValuesByNotebookField(NotebookField field) {
+        List<String> out = new ArrayList<>();
+        for (NoteBook nb : set) {
+            if (!out.contains(nb.getValue(field))) {
+                out.add(nb.getValue(field));
+            }
+        }
+        Collections.sort(out);
+        return out;
+    }
+
+    /**
+     * Метод для получения множества ноутбуков, удовлетворяющих заданному фильтру
+     * @param filter
+     * @return
+     */
+    private Set<NoteBook> getFiltered(NoteBookFilter filter) {
+        Set<NoteBook> res = new HashSet<>();
+        List<NotebookField> filterFields = filter.getUsedNotebookFields();
+        for (NoteBook nb : set) {
+            boolean addToResult = !filterFields.isEmpty();
+            for (NotebookField field : filterFields) {
+                addToResult = addToResult && filter.inFilter(field, nb.getValue(field));
+            }
+            if (addToResult) res.add(nb);
+        }
+        return res;
+    }
+
+    /**
+     * Метод для вывода заданного множества ноутбуков на экран
+     * @param outSet Множество ноутбуков для вывода
+     */
+    private void show(Set<NoteBook> outSet) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        for (NoteBook nb : outSet) {
+            sb.append(i++)
+                    .append(": ")
+                    .append(nb)
+                    .append("\n");
+        }
+        System.out.print(sb);
+    }
+}
