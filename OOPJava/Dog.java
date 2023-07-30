cd1419fc9e0575111c43514a9601cd27eb9cba31
chuprov-vd <chuprov271@gmail.com>
2023-07-30 22:24:26 +0300

    class Dog

--- OOPJava/Dog.java
+++ OOPJava/Dog.java
@@ -1,2 +1,11 @@
-package OOPJava;public class Dog {
+package OOPJava;
+
+public class Dog {
+    String name;
+    String breed;
+    int weight;
+
+    String resumeDog(){
+        return "Имя собаки: " + name + "\n" + "Порода собаки: " + breed + "\n" + "Вес собаки: " + weight + "кг.";
+    }
 }
