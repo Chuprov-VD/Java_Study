e274ea9477ed769ca2882019b550ebed9b00702f
chuprov-vd <chuprov271@gmail.com>
2023-08-02 01:31:08 +0300

    file Management Monstr

--- OOPJava/MainMonster.java
+++ OOPJava/MainMonster.java
@@ -1,2 +1,17 @@
-package OOPJava;public class MainMonster {
+package OOPJava;
+
+public class MainMonster {
+    public static void main(String[] args) {
+        Monster gera = new Monster(4);
+        gera.size();
+        gera.voise(4, "Arrr");
+
+        Monster gena = new Monster(5,4,4);
+        gena.size();
+        gena.voise();
+
+        System.out.println(gera.sizeMonster(gena));
+        Monster fedr = gera.copy();
+        fedr.size();
+    }
 }
